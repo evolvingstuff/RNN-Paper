6fb76c3fe3f6c37c9a8462c65ada6bc85fa88aeb
Thomas Lahore <tom.lahore@gmail.com>
2018-06-10 02:31:41 -0700

    Fixed bug in code, and also added more degrees of freedom to explore

--- Visualization/state-space.js
+++ Visualization/state-space.js
@@ -5,20 +5,22 @@ const HEIGHT = 500;
 const UNIT = 450;
 const OFFSET = 25;
 const MAX_DEPTH = 10;
+const NEG_INPUT = -0.1;
 
 function render() {
 
-	let alpha = parseFloat($('#inp_x').val());
-	let beta = parseFloat($('#inp_y').val())/1000;
+	let alpha = parseFloat($('#inp_alpha').val())/1000;
+	let beta = parseFloat($('#inp_beta').val())/1000;
+	let rec = parseFloat($('#inp_rec').val())/1000;
 
 	let ctx = document.getElementById('cnvs').getContext('2d');
 	ctx.fillStyle = 'white';
 	ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-	extend(ctx, 0, alpha, beta, 0);
+	extend(ctx, 0, alpha, beta, rec, 0);
 }
 
-function extend(ctx, loc, alpha, beta, depth) {
+function extend(ctx, loc, alpha, beta, rec, depth) {
 
 	if (depth >= MAX_DEPTH) {
 		return;
@@ -31,7 +33,7 @@ function extend(ctx, loc, alpha, beta, depth) {
 	ctx.strokeStyle = 'rgba(0,0,0,0.2)';
 	ctx.lineWidth = 1;
 	from = Math.abs(loc)*UNIT;
-	to = Math.abs(alpha + beta*loc)*UNIT;
+	to = Math.abs(alpha + rec*loc)*UNIT;
 	if (to < from) {
 		let tmp = to;
 		to = from;
@@ -43,14 +45,14 @@ function extend(ctx, loc, alpha, beta, depth) {
 	ctx.arc(x, y, r, Math.PI, 0);
 	ctx.stroke();
 
-	extend(ctx, Math.abs(alpha+beta*loc), alpha, beta, depth+1);
+	extend(ctx, Math.abs(alpha + rec*loc), alpha, beta, rec, depth+1);
 
 	//neg stroke
 	ctx.beginPath();
 	ctx.strokeStyle = 'rgba(255,0,0,0.2)';
 	ctx.lineWidth = 1;
 	from = Math.abs(loc)*UNIT;
-	to = Math.abs(beta*loc)*UNIT;
+	to = Math.abs(beta + rec*loc)*UNIT;
 	if (to < from) {
 		let tmp = to;
 		to = from;
@@ -63,12 +65,14 @@ function extend(ctx, loc, alpha, beta, depth) {
 	ctx.arc(x, y, r, 0, Math.PI);
 	ctx.stroke();
 
-	extend(ctx, Math.abs(beta*loc), alpha, beta, depth+1);
+	extend(ctx, Math.abs(beta + rec*loc), alpha, beta, rec, depth+1);
 }
 
 function init() {
 
-	$('#inp_y').on('input', render);
+	$('#inp_alpha').on('input', render);
+	$('#inp_beta').on('input', render);
+	$('#inp_rec').on('input', render);
 
 	render();
 }
